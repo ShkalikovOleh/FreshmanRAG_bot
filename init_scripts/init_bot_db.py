6ddd3a81c00b45fcd85d0da8b8a8c1848dc850f5
Oleh Shkalikov <Shkalikov.Oleh@gmail.com>
2024-08-11 21:59:12 +0200

    Add pgsql doc store init to the init db script. Add elasticsearch to docker-compose and requirements

--- init_scripts/init_bot_db.py
+++ init_scripts/init_bot_db.py
@@ -1,7 +1,8 @@
 import os
 
 import hydra
-from omegaconf import DictConfig
+from hydra.utils import instantiate
+from omegaconf import DictConfig, ListConfig
 from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
@@ -9,8 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from bot.db import Admin, Base
 
 
-@hydra.main(version_base="1.3", config_path="../configs", config_name="default")
-def main(config: DictConfig) -> None:
+def init_admin_db(config: DictConfig) -> None:
     father_tg_id = os.getenv("FATHER_TG_ID")
     father_tg_tag = os.getenv("FATHER_TG_TAG")
     engine = create_engine(config["bot_db_connection"])
@@ -30,7 +30,46 @@ def main(config: DictConfig) -> None:
             session.add(main_admin)
             session.commit()
     except IntegrityError:
-        print("Database has been already created and initialized!")
+        print("Admin database has been already created and initialized!")
+
+
+def init_pgsql_docstore(store_config: DictConfig):
+    docstore = instantiate(store_config, async_mode=False)
+    docstore.create_schema()
+
+
+# flake8: noqa: C901
+def finditems(obj, key):
+    found = []
+    if isinstance(obj, (dict, DictConfig)):
+        if key in obj:
+            found.append(obj[key])
+        for _, v in obj.items():
+            if isinstance(v, (dict, DictConfig)):
+                result = finditems(v, key)
+                if result is not None:
+                    found.extend(result)
+            elif isinstance(v, (list, ListConfig)):
+                for item in v:
+                    result = finditems(item, key)
+                    if result is not None:
+                        found.extend(result)
+    elif isinstance(obj, (list, ListConfig)):
+        for item in obj:
+            result = finditems(item, key)
+            if result is not None:
+                found.extend(result)
+    return list(set(found))
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="default")
+def main(config: DictConfig) -> None:
+    init_admin_db(config)
+
+    docstore_cfgs = finditems(config, "docstore")
+    for cfg in docstore_cfgs:
+        if cfg["_target_"] == "crag.storage.PGSQLDocStore":
+            init_pgsql_docstore(cfg)
 
 
 if __name__ == "__main__":
